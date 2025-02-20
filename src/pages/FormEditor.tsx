54c3a489abaa372d0c1f9ac3a450da93211a2fa2
F4E12 <76392444+F4E12@users.noreply.github.com>
2025-02-20 14:15:31 +0700

    Update FormEditor.tsx

--- src/pages/FormEditor.tsx
+++ src/pages/FormEditor.tsx
@@ -2,7 +2,7 @@ export default function FormEditor() {
   return (
     <div className="">
       This is form editor
-      <div className="text-destructive ">Title</div>
+      <div className="text-destructive font-bold">Title</div>
     </div>
   );
 }
