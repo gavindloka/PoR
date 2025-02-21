c6cdb50f79e4811ae16e72d9e912d9bfd8c5c3a8
gavindloka <lokagavindloka@gmail.com>
2025-02-21 20:42:31 +0700

    home

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -30,7 +30,7 @@ const HomePage = () => {
     hidden: { width: '0%' },
     visible: {
       width: '100%',
-      transition: { delay:2.5, duration: 0.8, ease: 'easeInOut' },
+      transition: { delay: 2.5, duration: 0.8, ease: 'easeInOut' },
     },
   };
 
@@ -47,7 +47,7 @@ const HomePage = () => {
 
   return (
     <>
-      <div className="w-full m-auto bg-transparent py-20">
+      <div className="w-full m-auto bg-transparent py-20 px-20">
         <motion.div
           className="flex flex-col justify-center text-center items-center font-satoshi font-bold"
           variants={containerVariants}
@@ -61,13 +61,14 @@ const HomePage = () => {
             with{' '}
             <motion.span className="relative inline-block">
               <motion.span
-                className="relative z-20"
-                initial={{ color: '#000000' }}
-                animate={{ color: '#16a34a' }}
+                className="relative z-20 bg-gradient-to-r from-green-500 via-green-600 to-green-800 bg-clip-text text-transparent"
+                initial={{ backgroundPosition: '0% 50%' }}
+                animate={{ backgroundPosition: '100% 50%' }}
                 transition={{ duration: 1, delay: 1 }}
               >
                 Decentralized Survey
               </motion.span>
+
               <motion.span
                 className="absolute left-0 bottom-0 h-1 bg-yellow-300"
                 style={{ width: '100%' }}
@@ -78,13 +79,14 @@ const HomePage = () => {
             </motion.span>
           </motion.p>
           <motion.p
-            className="mt-6 text-xl font-light text-gray-500"
+            className="mt-6 text-xl font-light text-gray-500 w-2/6"
             variants={itemVariants}
           >
-            Empowering Research, Preserving Knowledge
+            Build and complete surveys with transparency, as all data is
+            securely stored on the blockchain
           </motion.p>
           <motion.div className="mt-8 flex gap-5" variants={itemVariants}>
-            <Button className=" font-bold rounded-lg bg-green-600 w-32 h-11 hover:bg-green-700 hover:scale-105 transition-all">
+            <Button className=" font-bold rounded-lg bg-green-600 w-32 h-11 hover:border-0 hover:bg-green-700 hover:scale-105 transition-all">
               Design a Survey
             </Button>
             <Button
@@ -93,6 +95,64 @@ const HomePage = () => {
             >
               Contribute
             </Button>
+          </motion.div>
+          <motion.div className="w-full flex items-end justify-center gap-10">
+            <motion.div
+              className="w-48 h-64 bg-green-700 rounded-xl p-4 flex flex-col gap-3 justify-center"
+              initial={{ opacity: 0, x: -50 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.5, delay: 2 }}
+            >
+              <p className="font-bold text-xl text-white">Create Your Survey</p>
+              <p className="text-white font-light">
+                Design decentralized surveys powered by ICP
+              </p>
+            </motion.div>
+
+            <motion.div
+              className="w-48 h-52 bg-green-600 rounded-xl p-4 flex flex-col gap-3 justify-center"
+              initial={{ opacity: 0, x: -50 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.5, delay: 2 }}
+            >
+              <p className="font-bold text-xl text-white">Create Your Survey</p>
+              <p className="text-white font-light">
+                Design decentralized surveys powered by ICP
+              </p>
+            </motion.div>
+
+            <motion.div
+              className="w-48 h-40 bg-white rounded-xl border-2 p-4 flex flex-col justify-center"
+              initial={{ opacity: 0, x: -50 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.5, delay: 2 }}
+            >
+              <p>#100</p>
+            </motion.div>
+
+            <motion.div
+              className="w-48 h-52 bg-green-600 rounded-xl p-4 flex flex-col gap-3 justify-center"
+              initial={{ opacity: 0, x: 50 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.5, delay: 2.3 }}
+            >
+              <p className="font-bold text-xl text-white">Create Your Survey</p>
+              <p className="text-white font-light">
+                Design decentralized surveys powered by ICP
+              </p>
+            </motion.div>
+
+            <motion.div
+              className="w-48 h-64 bg-green-700 rounded-xl p-4 flex flex-col gap-3 justify-center"
+              initial={{ opacity: 0, x: 50 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.5, delay: 2.3 }}
+            >
+              <p className="font-bold text-xl text-white">Create Your Survey</p>
+              <p className="text-white font-light">
+                Design decentralized surveys powered by ICP
+              </p>
+            </motion.div>
           </motion.div>
         </motion.div>
       </div>
