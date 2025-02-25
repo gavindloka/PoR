d3dd930ef0e86ab3ab3751d036e42e4f39364079
nenlitiochristian <nenlitiochristian@gmail.com>
2025-02-25 09:51:13 +0700

    chore: update from wasm32-wasi to wasm32-wasip1
    wasm32-wasi is being renamed to wasm32-wasip1 according to this github post https://github.com/scylladb/scylladb/issues/20878

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -ex
 
 export RUSTFLAGS=$RUSTFLAGS' -C target-feature=+simd128'
-cargo build --release --target=wasm32-wasi
-wasi2ic ./target/wasm32-wasi/release/backend_ai.wasm ./target/wasm32-wasi/release/backend_ai-ic.wasm
-wasm-opt -Os --enable-simd --enable-bulk-memory   -o ./target/wasm32-wasi/release/backend_ai-ic.wasm \
-        ./target/wasm32-wasi/release/backend_ai-ic.wasm
+cargo build --release --target=wasm32-wasip1
+wasi2ic ./target/wasm32-wasip1/release/backend_ai.wasm ./target/wasm32-wasip1/release/backend_ai-ic.wasm
+wasm-opt -Os --enable-simd --enable-bulk-memory   -o ./target/wasm32-wasip1/release/backend_ai-ic.wasm \
+        ./target/wasm32-wasip1/release/backend_ai-ic.wasm
